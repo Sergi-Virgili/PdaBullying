4c1b2e525e129763d83d3617de885de5ad791cc8
Sergi Virgili <svirgilif@gmail.com>
2019-11-29 09:59:25 +0100

    Resource Migrate

---
+++ database/migrations/2019_11_29_083529_create_resources_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            
+            $table->string('logoUrl')->nullable();
+            $table->string('link')->nullable();
+            $table->integer('type_id')->nullable();
+            $table->string('adress')->nullable();
+            $table->integer('lat');
+            $table->integer('lng');
+            
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
