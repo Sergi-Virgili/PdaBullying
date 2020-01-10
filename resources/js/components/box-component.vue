b8300111bed02a75a63927b2be7f6ff7def83e80
jani1977 <azuladito12@hotmail.com>
2020-01-10 09:37:53 +0100

    checkboxbasico

--- resources/js/components/box-component.vue
+++ resources/js/components/box-component.vue
@@ -1,28 +1,38 @@
 <template>
-  <v-app>
-    <v-card fluid>
-      <v-list dense>
-        <v-checkbox color="grey" v-model="selected" label="Type1" value="Type1"></v-checkbox>
-        <v-checkbox color="grey" v-model="selected" label="Type2" value="Type2"></v-checkbox>
-        <v-checkbox color="grey" v-model="selected" label="Type3" value="Type3"></v-checkbox>
-        <v-checkbox color="grey" v-model="selected" label="Type4" value="Type4"></v-checkbox>
-      </v-list>
-    </v-card>
-  </v-app>
+    <v-app>
+        <v-card fluid>
+            <v-list dense>
+                <v-list-item>
+                    <v-checkbox
+                        color="grey"
+                        v-model="selected"
+                        label="Type1"
+                        value="Type1"
+                    ></v-checkbox>
+                </v-list-item>
+                <v-list-item>
+                    <v-checkbox
+                        color="grey"
+                        v-model="selected"
+                        label="Type2"
+                        value="Type2"
+                    ></v-checkbox>
+                </v-list-item>
+            </v-list>
+        </v-card>
+    </v-app>
 </template>
-
-
 
 <script>
 export default {
-  data() {
-    return {};
-  }
+    data() {
+        return {};
+    }
 };
 </script>
 
 <style>
 h1 {
-  background-color: black;
+    background-color: black;
 }
 </style>
