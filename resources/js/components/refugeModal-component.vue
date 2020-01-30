4b02da4048573edbb665f932034fcc264961bfa2
Sergi Virgili <svirgilif@gmail.com>
2020-01-30 23:52:31 +0100

    autogrow text

--- resources/js/components/refugeModal-component.vue
+++ resources/js/components/refugeModal-component.vue
@@ -1,162 +1,233 @@
 <template>
-<div>
-  
-    <v-card >
-    <v-card-title class="pb-0">
-      <img :src="refugeSelected.logoUrl" />
-    </v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-text-field label="Título" v-if="editMode" v-model="refugeSelected.name"/>
-        <p v-if="!editMode">{{ refugeSelected.name }}</p>
-        
-        
-        <v-textarea
-       v-if="editMode"
-        v-model="refugeSelected.description"
-        label="Descripción"
-        rows="1"
-        
-        ></v-textarea>
-        <p v-if="!editMode">{{ refugeSelected.description }}</p>
-          
-     
-        <v-text-field v-if="!editMode" disabled label="Email" prepend-icon="mdi-email-outline" v-model="refugeSelected.email" />
-        <v-text-field v-if="editMode" label="Email" prepend-icon="mdi-email-outline" v-model="refugeSelected.email" />
-        <v-text-field v-if="!editMode" disabled label="Teléfono" prepend-icon="mdi-phone-outline" v-model="refugeSelected.phone"/>
-        <v-text-field v-if="editMode" label="Teléfono" prepend-icon="mdi-phone-outline" v-model="refugeSelected.phone"/>
-        <v-text-field v-if="!editMode" disabled label="Calle" prepend-icon="mdi-map-marker-outline" v-model="refugeSelected.road"/>
-        <v-text-field v-if="editMode" label="Calle" prepend-icon="mdi-map-marker-outline" v-model="refugeSelected.road"/>
-        <v-text-field v-if="!editMode" disabled label="Numero" prepend-icon="mdi-home-map-marker" v-model="refugeSelected.house_number"/>
-        <v-text-field v-if="editMode" label="Numero" prepend-icon="mdi-home-map-marker" v-model="refugeSelected.house_number"/>
-        <v-text-field v-if="!editMode" disabled label="Ciudad" prepend-icon="mdi-map-outline" v-model="refugeSelected.city"/>
-        <v-text-field v-if="editMode" label="Ciudad" prepend-icon="mdi-map-outline" v-model="refugeSelected.city"/>
+    <div>
+        <v-card>
+            <v-card-title class="pb-0">
+                <img :src="refugeSelected.logoUrl" />
+            </v-card-title>
+            <v-card-text>
+                <v-form>
+                    <v-text-field
+                        label="Título"
+                        v-if="editMode"
+                        v-model="refugeSelected.name"
+                    />
+                    <p v-if="!editMode">{{ refugeSelected.name }}</p>
 
-    
-        <!-- <v-text-field
+                    <v-textarea
+                        v-if="editMode"
+                        v-model="refugeSelected.description"
+                        label="Descripción"
+                        rows="1"
+                        auto-grow
+                    ></v-textarea>
+                    <p v-if="!editMode">{{ refugeSelected.description }}</p>
+
+                    <v-text-field
+                        v-if="!editMode"
+                        disabled
+                        label="Email"
+                        prepend-icon="mdi-email-outline"
+                        v-model="refugeSelected.email"
+                    />
+                    <v-text-field
+                        v-if="editMode"
+                        label="Email"
+                        prepend-icon="mdi-email-outline"
+                        v-model="refugeSelected.email"
+                    />
+                    <v-text-field
+                        v-if="!editMode"
+                        disabled
+                        label="Teléfono"
+                        prepend-icon="mdi-phone-outline"
+                        v-model="refugeSelected.phone"
+                    />
+                    <v-text-field
+                        v-if="editMode"
+                        label="Teléfono"
+                        prepend-icon="mdi-phone-outline"
+                        v-model="refugeSelected.phone"
+                    />
+                    <v-text-field
+                        v-if="!editMode"
+                        disabled
+                        label="Calle"
+                        prepend-icon="mdi-map-marker-outline"
+                        v-model="refugeSelected.road"
+                    />
+                    <v-text-field
+                        v-if="editMode"
+                        label="Calle"
+                        prepend-icon="mdi-map-marker-outline"
+                        v-model="refugeSelected.road"
+                    />
+                    <v-text-field
+                        v-if="!editMode"
+                        disabled
+                        label="Numero"
+                        prepend-icon="mdi-home-map-marker"
+                        v-model="refugeSelected.house_number"
+                    />
+                    <v-text-field
+                        v-if="editMode"
+                        label="Numero"
+                        prepend-icon="mdi-home-map-marker"
+                        v-model="refugeSelected.house_number"
+                    />
+                    <v-text-field
+                        v-if="!editMode"
+                        disabled
+                        label="Ciudad"
+                        prepend-icon="mdi-map-outline"
+                        v-model="refugeSelected.city"
+                    />
+                    <v-text-field
+                        v-if="editMode"
+                        label="Ciudad"
+                        prepend-icon="mdi-map-outline"
+                        v-model="refugeSelected.city"
+                    />
+
+                    <!-- <v-text-field
           :type="showPassword ? 'text' : 'password'"
           label="Confirma Password"
           prepend-icon="mdi-lock-outline"
           :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
           @click:append="showPassword = !showPassword"
         /> -->
-      </v-form>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-btn class="rounded" 
-        @click="editMode = !editMode"
-        v-if="!editMode" 
-        tile 
-        outlined 
-        color="success">
-        <span>Edit</span>
-      </v-btn>
-      <v-btn class="rounded" 
-        
-        v-if="!editMode" 
-        tile 
-        outlined 
-        color="success"
-        @click="attachRefuge">
-        <span>Import</span>
-      </v-btn>
-      <v-btn class="rounded" 
-        
-        v-if="!editMode" 
-        tile 
-        outlined 
-        color="success"
-        @click="detachRefuge">
-        <span>Remove</span>
-      </v-btn>
-      <v-btn class="rounded" 
-        @click="OnClickDelete()"
-        v-if="!editMode" tile outlined 
-        color="error">
-        <span>Delete</span>
-      </v-btn>
-      <v-btn class="rounded" 
-        @click="editMode = !editMode"
-        
-        v-if="editMode" 
-        tile 
-        outlined 
-        color="primary">
-        <span>Cancel</span>
-      </v-btn>
-      <v-btn class="rounded" 
-        v-if="editMode" tile outlined
-        @click="OnClickUpdate()" 
-        color="success">
-        <span>Ok</span>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-</div>
+                </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn
+                    class="rounded"
+                    @click="editMode = !editMode"
+                    v-if="!editMode"
+                    tile
+                    outlined
+                    color="success"
+                >
+                    <span>Edit</span>
+                </v-btn>
+                <v-btn
+                    class="rounded"
+                    v-if="!editMode"
+                    tile
+                    outlined
+                    color="success"
+                    @click="attachRefuge"
+                >
+                    <span>Import</span>
+                </v-btn>
+                <v-btn
+                    class="rounded"
+                    v-if="!editMode"
+                    tile
+                    outlined
+                    color="success"
+                    @click="detachRefuge"
+                >
+                    <span>Remove</span>
+                </v-btn>
+                <v-btn
+                    class="rounded"
+                    @click="OnClickDelete()"
+                    v-if="!editMode"
+                    tile
+                    outlined
+                    color="error"
+                >
+                    <span>Delete</span>
+                </v-btn>
+                <v-btn
+                    class="rounded"
+                    @click="editMode = !editMode"
+                    v-if="editMode"
+                    tile
+                    outlined
+                    color="primary"
+                >
+                    <span>Cancel</span>
+                </v-btn>
+                <v-btn
+                    class="rounded"
+                    v-if="editMode"
+                    tile
+                    outlined
+                    @click="OnClickUpdate()"
+                    color="success"
+                >
+                    <span>Ok</span>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <script>
 export default {
-   props: ["refugeSelected"],
-  data() {
-    return {
-      //refugeSelected: {},
-      editMode: false,
-      
-    };
-  },
-  
-  methods: {
-    attachRefuge() {
-      axios.get(`/api/maps/attach/${this.refugeSelected.id}`).then(response => console.log('attached'));
-      //this.$emit("attachRefuge", this.refugeSelected.id);
+    props: ["refugeSelected"],
+    data() {
+        return {
+            //refugeSelected: {},
+            editMode: false
+        };
     },
-    OnClickEdit(){
-      this.editMode = true
-      
-    },
-    OnClickDelete(){
-      axios.delete(`/api/refuges/${this.refuge.id}`).then(()=> {
-        this.editMode = false
-        // this.$emit("delete")
-      })
-      
-    },
-    OnClickUpdate() {
-      const params = {
-        name: this.refugeSelected.name,
-        description: this.refugeSelected.description,
-        road: this.refugeSelected.road,
-        hpuse_number: this.refugeSelected.house_number,
-        city: this.refugeSelected.city,
-        email: this.refugeSelected.email,
-        phone: this.refugeSelected.phone,
-      };
-      console.log(params)
-      axios.put(`/api/refuges/${this.refugeSelected.id}`, params).then((response) => {
-      this.editMode = false;
-      const refuge = response.data;
-      console.log(response.data)
-      this.$emit('update', refuge);
-      });
-    },
-    
-    detachRefuge() {
-      axios.get(`/api/maps/detach/${this.refugeSelected.id}`).then(response => {
-        this.$emit("detachRefuge", this.refugeSelected.id);
-      });
+
+    methods: {
+        attachRefuge() {
+            axios
+                .get(`/api/maps/attach/${this.refugeSelected.id}`)
+                .then(response => console.log("attached"));
+            //this.$emit("attachRefuge", this.refugeSelected.id);
+        },
+        OnClickEdit() {
+            this.editMode = true;
+        },
+        OnClickDelete() {
+            axios.delete(`/api/refuges/${this.refuge.id}`).then(() => {
+                this.editMode = false;
+                // this.$emit("delete")
+            });
+        },
+        OnClickUpdate() {
+            const params = {
+                name: this.refugeSelected.name,
+                description: this.refugeSelected.description,
+                road: this.refugeSelected.road,
+                hpuse_number: this.refugeSelected.house_number,
+                city: this.refugeSelected.city,
+                email: this.refugeSelected.email,
+                phone: this.refugeSelected.phone
+            };
+            console.log(params);
+            axios
+                .put(`/api/refuges/${this.refugeSelected.id}`, params)
+                .then(response => {
+                    this.editMode = false;
+                    const refuge = response.data;
+                    console.log(response.data);
+                    this.$emit("update", refuge);
+                });
+        },
+
+        detachRefuge() {
+            axios
+                .get(`/api/maps/detach/${this.refugeSelected.id}`)
+                .then(response => {
+                    this.$emit("detachRefuge", this.refugeSelected.id);
+                });
+        }
     }
-  }
 };
 </script>
 
 <style>
 h3 {
-  color: orange;
-  padding: 1%;
+    color: orange;
+    padding: 1%;
 }
 .logo {
-  width: 120px;
+    width: 120px;
 }
 </style>
--- resources/js/views/admin/Publish.vue
+++ resources/js/views/admin/Publish.vue
@@ -1,141 +1,145 @@
 <template>
-  <section class="publishTable row">
-    <table class="table table-hover col-md-6">
-      <thead>
-        <tr>
-          <th scope="col">Recurso</th>
-          <th scope="col">Fecha</th>
-          <th scope="col">Acción</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(refuge, index) in refuges"
-          :key="index"
-          
-        >
-        <v-dialog v-model="dialog" width="600px" style='z-index:100000001' >
-                  
-                  <refugeModal-component
-                      
-                     :refugeSelected="refuge"
-                      @update="updateRefuge(index, ...arguments)"
-                     
-                    ></refugeModal-component>
-    </v-dialog>
-          <td>{{refuge.name}}</td>
-          <td>{{refuge.created_at}}</td>
-          <td>
-       
-          <v-tooltip top v-if="!refuge.is_Public">
-          <template v-slot:activator="{ on }" >
-            <v-btn
-             v-on="on"
-              class="ma-2"
-              fab
-              color="success"
-              dark
-              @click="onClickPublish(index)"
-              x-small
-            >
-            <v-icon>mdi-publish</v-icon>
-          </v-btn>
-          </template>
-          <span>Publicar</span>
-          </v-tooltip>
-           
-           <v-tooltip top v-if="refuge.is_Public">
-          <template v-slot:activator="{ on }" >
-            <v-btn
-              class="ma-2"
-              fab
-              v-on="on"
-              color="blue darken-2"
-              dark
-              @click="onClickHidde(index)"
-              x-small
-            >
-            <v-icon>mdi-eye-off</v-icon>
-            </v-btn>
-            </template>
-          <span>Ocultar</span>
-          </v-tooltip>
-            
+    <section class="publishTable row">
+        <table class="table table-hover col-md-6">
+            <thead>
+                <tr>
+                    <th scope="col">Recurso</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Acción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(refuge, index) in refuges" :key="index">
+                    <v-dialog v-model="dialog" width="600px" style="z-index:10">
+                        <refugeModal-component
+                            :refugeSelected="refuge"
+                            @update="updateRefuge(index, ...arguments)"
+                        ></refugeModal-component>
+                    </v-dialog>
+                    <td>{{ refuge.name }}</td>
+                    <td>{{ refuge.created_at }}</td>
+                    <td>
+                        <v-tooltip top v-if="!refuge.is_Public">
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                    v-on="on"
+                                    class="ma-2"
+                                    fab
+                                    color="success"
+                                    dark
+                                    @click="onClickPublish(index)"
+                                    x-small
+                                >
+                                    <v-icon>mdi-publish</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Publicar</span>
+                        </v-tooltip>
 
-             <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              class="ma-2"
-              v-on="on"
-              fab
-              color="orange darken-2"
-              dark
-              @click="openDialogRefuge"
-              x-small
-            >
-            <v-icon>mdi-information</v-icon>
-            </v-btn>
-            </template>
-          <span>Información</span>
-          </v-tooltip>
+                        <v-tooltip top v-if="refuge.is_Public">
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                    class="ma-2"
+                                    fab
+                                    v-on="on"
+                                    color="blue darken-2"
+                                    dark
+                                    @click="onClickHidde(index)"
+                                    x-small
+                                >
+                                    <v-icon>mdi-eye-off</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Ocultar</span>
+                        </v-tooltip>
 
-          <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <v-btn class="ma-2" v-on="on" fab color="red" dark @click="onClickDelete(index)" x-small>
-              <v-icon>mdi-trash-can-outline</v-icon>
-            </v-btn>
-            </template>
-          <span>Eliminar</span>
-          </v-tooltip>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    
-    <publicMap-component style= "z-index: 1" class="col-md-6" :refuges="refuges"></publicMap-component>
-  </section>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                    class="ma-2"
+                                    v-on="on"
+                                    fab
+                                    color="orange darken-2"
+                                    dark
+                                    @click="openDialogRefuge"
+                                    x-small
+                                >
+                                    <v-icon>mdi-information</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Información</span>
+                        </v-tooltip>
+
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                    class="ma-2"
+                                    v-on="on"
+                                    fab
+                                    color="red"
+                                    dark
+                                    @click="onClickDelete(index)"
+                                    x-small
+                                >
+                                    <v-icon>mdi-trash-can-outline</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Eliminar</span>
+                        </v-tooltip>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <publicMap-component
+            style="z-index: 1"
+            class="col-md-6"
+            :refuges="refuges"
+        ></publicMap-component>
+    </section>
 </template>
 
 <script>
 export default {
-  name: "Publish",
-  data() {
-    return {
-      refuges: [],
-      dialog:false
-    };
-  },
-  created() {
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      axios.get("/api/refuges").then(response => {
-        this.refuges = response.data.refuge;
-      });
+    name: "Publish",
+    data() {
+        return {
+            refuges: [],
+            dialog: false
+        };
     },
-    onClickPublish(index) {
-      let data = { id: this.refuges[index].id };
-      axios.patch("/api/refuges/publish", data).then(response => {
-        console.log(response);
-        this.refuges[index].is_Public = 1;
-      });
+    created() {
+        this.fetchData();
     },
-    onClickHidde(index) {
-      let data = { id: this.refuges[index].id };
-      axios.patch("/api/refuges/hidde", data).then(response => {
-        this.refuges[index].is_Public = 0;
-      });
-    },
-    onClickDelete(index) {
-      
-      axios.delete(`/api/refuges/${this.refuges[index].id}`).then(response => {
-        this.refuges.splice(index, 1);
-      });
-    },
-    openDialogRefuge() {
-      this.dialog = true
+    methods: {
+        fetchData() {
+            axios.get("/api/refuges").then(response => {
+                this.refuges = response.data.refuge;
+            });
+        },
+        onClickPublish(index) {
+            let data = { id: this.refuges[index].id };
+            axios.patch("/api/refuges/publish", data).then(response => {
+                console.log(response);
+                this.refuges[index].is_Public = 1;
+            });
+        },
+        onClickHidde(index) {
+            let data = { id: this.refuges[index].id };
+            axios.patch("/api/refuges/hidde", data).then(response => {
+                this.refuges[index].is_Public = 0;
+            });
+        },
+        onClickDelete(index) {
+            axios
+                .delete(`/api/refuges/${this.refuges[index].id}`)
+                .then(response => {
+                    this.refuges.splice(index, 1);
+                });
+        },
+        openDialogRefuge() {
+            this.dialog = true;
+        }
     }
-  }
 };
 </script>
