82da68558465d0da32598cefa73b8a17b362a880
Sergi Virgili <svirgilif@gmail.com>
2020-02-12 12:45:09 +0100

    Delete users front added

--- resources/js/views/admin/Users.vue
+++ resources/js/views/admin/Users.vue
@@ -1,8 +1,8 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="desserts"
-    sort-by="calories"
+    :items="users"
+    sort-by="name"
     class="elevation-1"
   >
     <template v-slot:top>
@@ -27,7 +27,7 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                    <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
@@ -90,24 +90,23 @@
         { text: 'Apellido', value: 'surname' },
         { text: 'Email', value: 'email' },
         { text: 'Teléfono', value: 'telf' },
-        { text: 'Protein (g)', value: 'protein' },
+       
         { text: 'Actions', value: 'action', sortable: false },
       ],
-      desserts: [],
+      users: [],
       editedIndex: -1,
       editedItem: {
         name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        surname: '',
+        email: '',
+        telf: '',
+        
       },
       defaultItem: {
         name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        surname: '',
+        email: '',
+        telf: '',
       },
     }),
 
@@ -129,78 +128,10 @@
 
     methods: {
       initialize () {
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
-        ]
+        axios.get('/api/users/').then(response => {
+          this.users = response.data
+          console.log(this.users)
+        })
       },
 
       editItem (item) {
@@ -210,8 +141,15 @@
       },
 
       deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+        const index = this.users.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.destroy(index)
+      },
+
+      destroy(index) {
+        axios.delete('/api/users/'+this.users[index].id).then(respose => {
+
+          this.users.splice(index, 1)
+        })
       },
 
       close () {
